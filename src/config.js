0ab3a0403b973762aba9f135d903c55c653cae32
Don Siddell <dsiddell@cryoport.com>
2025-02-19 21:50:26 -0500

    update dockerfile

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@
 const CONFIG = {
     companyName: "MVE Biological Solutions",
     productName: "BIOCONNECT",
-    headerImage: "src/assets/cryo-logo.png", // Replace with actual URL
+    headerImage: "./src/assets/cryo-logo.png", // Replace with actual URL
     contactEmail: "mailto:support@mvebioconnect.com",
     contactPhone: "tel:+014043831900",
-    registerNewUser: "https://dev-wdd3veon1ju6jgpy.us.auth0.com",
-    registerDevice: "https://dev-wdd3veon1ju6jgpy.us.auth0.com"
+    registerNewUser: "https://dev-wdd3veon1ju6jgpy.us.auth0.com/u/signup",
+    registerDevice: "https://dev-wdd3veon1ju6jgpy.us.auth0.com/u/login"
 };
 
 export default CONFIG;
