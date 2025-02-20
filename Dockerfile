b8dc11979df9a7badd74588fb76681b6d48578a9
Don Siddell <dsiddell@cryoport.com>
2025-02-19 20:03:12 -0500

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,26 @@
-// Dockerfile
-# Switch to the created non-root user
-USER 10001
+# Dockerfile
 
+# Use an official Node.js runtime as a parent image
 FROM node:18-alpine
+
+# Set working directory
 WORKDIR /app
+
+# Copy package.json and install dependencies
 COPY package.json .
 RUN npm install
+
+# Copy the rest of the application code
 COPY . .
+
+# Build the application
 RUN npm run build
-CMD ["npx", "serve", "-s", "build"]
+
+# Switch to a non-root user after all installations and builds
+USER 10001
+
+# Expose the application port
 EXPOSE 8080
+
+# Command to start the application
+CMD ["npx", "serve", "-s", "build"]
--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 // config.js
 const CONFIG = {
-    companyName: "Your Company Name",
-    productName: "Your Product Name",
-    headerImage: "https://yourdomain.com/header-image.png", // Replace with actual URL
-    contactEmail: "mailto:support@yourcompany.com",
-    contactPhone: "tel:+1234567890",
-    registerNewUser: "https://yourdomain.com/register-new-user",
-    registerDevice: "https://yourdomain.com/register-device"
+    companyName: "MVE Biological Solutions",
+    productName: "BIOCONNECT",
+    headerImage: "src/assets/cryo-logo.png", // Replace with actual URL
+    contactEmail: "mailto:support@mvebioconnect.com",
+    contactPhone: "tel:+014043831900",
+    registerNewUser: "https://dev-wdd3veon1ju6jgpy.us.auth0.com",
+    registerDevice: "https://dev-wdd3veon1ju6jgpy.us.auth0.com"
 };
 
 export default CONFIG;
