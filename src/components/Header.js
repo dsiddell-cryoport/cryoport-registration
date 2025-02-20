b1cdb34044adf158f6a0335032071621d561be8e
Don Siddell <dsiddell@cryoport.com>
2025-02-20 14:20:24 -0500

    fix logo issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import CONFIG from '../config';
 //import logo from '../assets/cryoport-svg.svg';
 
 const strLogo = CONFIG.headerImage;
-import logo from `${strLogo}`;
+import logo from '${strLogo}';
 
 function Header() {
     return (
