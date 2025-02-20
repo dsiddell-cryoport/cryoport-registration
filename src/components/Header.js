c2345652ec712128094e6d8b5e94ffd445c82987
Don Siddell <dsiddell@cryoport.com>
2025-02-20 14:23:55 -0500

    fix logo issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 // Header.js
 import React from 'react';
 import CONFIG from '../config';
-//import logo from '../assets/cryoport-svg.svg';
 
+//import logo from '../assets/cryoport-svg.svg';
 const strLogo = CONFIG.headerImage;
-import logo from '${strLogo}';
+//import logo from '${strLogo}';
 
 function Header() {
     return (
         <header className="header">
-            <img src={logo} alt="Company Logo" className="header-logo" />
+            <img src={strLogo} alt="Company Logo" className="header-logo" />
             <div className="header-text">
                 <h1>Welcome to {CONFIG.companyName} {CONFIG.productName}!</h1>
                 <p className="header-subtext">Please choose from the options below to register your device for real-time monitoring.</p>
